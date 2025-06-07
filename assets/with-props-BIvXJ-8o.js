4ae96d7e3a2087538261a63dc67acb15f5816e14
xt3362 <yukkuriyden@gmail.com>
2025-06-07 09:30:15 +0900

    Deploy: 2025-06-07 09:30:15

---
+++ assets/with-props-BIvXJ-8o.js
@@ -0,0 +1,1 @@
+import{t as p,v as t,w as e,x as o,a as s,y as u}from"./chunk-D4RADZKF-FHtUOQf9.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
